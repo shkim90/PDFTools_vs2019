857fc8f6a9fd45abc3f6e834300f97a114b9209d
shkim90 <shkim@inforad.co.kr>
2025-09-03 13:55:48 +0900

    초기 커밋.

---
+++ PdfMaker/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Office.Interop.Excel;
+using System;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length < 2)
+        {
+            Console.WriteLine("Usage: PdfMaker <ExcelFile> <PdfFile>");
+            return;
+        }
+        var excelFilePath = args[0];
+        var pdfFilePath = args[1];
+
+        var app = new Application
+        {
+            Visible = false
+        };
+        var workbook = app.Workbooks.Open(excelFilePath);
+        workbook.ExportAsFixedFormat(XlFixedFormatType.xlTypePDF, pdfFilePath);
+        Console.WriteLine($"{excelFilePath} converted to {pdfFilePath}");
+    }
+}
---
+++ PdfSigner/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Drawing;
+using Syncfusion.Licensing;
+using Syncfusion.Pdf.Graphics;
+using Syncfusion.Pdf.Parsing;
+using System;
+using System.IO;
+using System.Linq;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length < 3)
+        {
+            Console.WriteLine("Usage: PdfSigner <PdfFile> <SignatureFile> <SignatureLeft>,<SignatureTop>,<SignatureWidth,<SignatureHeight>");
+            return;
+        }
+        var pdfFilePath = args[0];
+        var signatureFilePath = args[1];
+        var signatureRect = new RectangleF();
+        try
+        {
+            var values = args[2].Split(',').Select(value => float.Parse(value)).ToArray();
+            signatureRect = new RectangleF(values[0], values[1], values[2], values[3]);
+        }
+        catch
+        {
+            Console.WriteLine("Wrong signature rectangle");
+            return;
+        }
+
+        SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1JEaF5cXmRCf1FpRmJGdld5fUVHYVZUTXxaS00DNHVRdkdmWXdecnVXQ2lcVUJwX0JWYEk=");
+
+        using (var doc = new PdfLoadedDocument(File.ReadAllBytes(pdfFilePath)))
+        {
+            var page = doc.Pages[0];
+            using (var signatureFileStream = File.OpenRead(signatureFilePath))
+            {
+                var signatureImage = PdfImage.FromStream(signatureFileStream);
+                page.Graphics.DrawImage(signatureImage, signatureRect);
+            }
+            using (var reportFileStream = File.Create(pdfFilePath))
+            {
+                doc.Save(reportFileStream);
+            }
+        }
+        Console.WriteLine($"{signatureFilePath} put on {pdfFilePath}");
+    }
+}
